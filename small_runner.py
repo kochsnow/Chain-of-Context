7766bef2c5f53e6711ca6ee2bea639b33b09d010
Shayan-P <shayanpardis82@gmail.com>
2023-12-05 12:04:35 -0500

    tweak prompt

--- small_runner.py
+++ small_runner.py
@@ -29,24 +29,29 @@ args = HfArgumentParser([runner_args, hf_args, oai_args, gen_args]).parse_args()
 
 task_name = "proofwriter-neurosymbolic-2shot"
 
+args.task_name = task_name
+
 args.max_length_generation = 3000
 args.temperature = 0.8
-args.openai_api_env_keys = ['OPENAI_API_KEY', 'OPENAI_API_KEY2', 'OPENAI_API_KEY3', 'OPENAI_API_KEY4']
+args.openai_api_env_keys = ['OPENAI_API_KEY', 'OPENAI_API_KEY2', 'OPENAI_API_KEY3', 'OPENAI_API_KEY4', 'OPENAI_API_KEY5', 'OPENAI_API_KEY6']
 args.model = 'gpt-3.5-turbo-16k-0613'  # 'gpt-3.5-turbo'  # hard coded model here
 args.allow_code_execution = True
 
 # todo remove debug suffix from here later
-run_id = f"{args.model}_${task_name}_debug"
+run_id = f"{args.model}_${task_name}_debug_ates_new_prompt_2_"
 args.save_generations_raw = True
 args.save_generations_prc = True
 args.save_references = True
 args.save_results = True
 args.save_context = True
+args.save_dataset_indices = True
+
 args.save_results_path = f'{run_id}_results.json'
 args.save_context_path = f'{run_id}_context.json'
 args.save_references_path = f'{run_id}_references.json'
 args.save_generations_raw_path = f'{run_id}_generations_raw.json'
 args.save_generations_prc_path = f'{run_id}_generations_prc.json'
+args.save_dataset_indices_path = f'{run_id}_dataset_indices.json'
 args.output_dir = 'outputs'
 
 args.output_dir = pathlib.Path(os.getcwd()) / args.output_dir
@@ -55,11 +60,12 @@ args.save_generations_prc_path = args.output_dir / args.save_generations_prc_pat
 args.save_references_path = args.output_dir / args.save_references_path
 args.save_results_path = args.output_dir / args.save_results_path
 args.save_context_path = args.output_dir / args.save_context_path
+args.save_dataset_indices_path = args.output_dir / args.save_dataset_indices_path
 args.save_generations_raw_path.parent.mkdir(parents=True, exist_ok=True)
 args.save_generations_prc_path.parent.mkdir(parents=True, exist_ok=True)
 args.save_references_path.parent.mkdir(parents=True, exist_ok=True)
 args.save_results_path.parent.mkdir(parents=True, exist_ok=True)
-
+args.save_dataset_indices_path.parent.mkdir(parents=True, exist_ok=True)
 
 my_parent_task = get_task(task_name)
 
@@ -105,29 +111,34 @@ You will be given the premises for a first-order logic (FOL) problem.
 The problem is to identify additional premises that are implicitly common sense from the ones given.
 The premises are given in the form of a set of first-order logic sentences.
 The task is to generate new common sense premises, text and FOL pairs, that would be common sense to someone reading the original premises.
+These new common sense premises should reflect the nature of synonyms and antonyms, categorize proper names, and identify implicit characteristics from the ones provided.
+Do not limit the amount of new premises generated in the output.
 Expressions should be adhere to the format of the Python NLTK package logic module. Here are a couple examples:
-        
 <INPUT>
 <PREMISES>
-Premise: When a person reads a book, that person gains knowledge.
-FOL: all x. all y. (Person(x) & Reads(x, y) & Book(y) -> Gains(x, Knowledge))
-Premise: Harry read the book "Walden" by Henry Thoreau.
+Premise: When a person reads a book, that person
+gains knowledge.
+FOL: all x. all y. (Person(x) & Reads(x, y) &
+Book(y) -> Gains(x, Knowledge))
+Premise: Harry read the book "Walden" by Henry
+Thoreau.
 FOL: Reads(Harry, Walden)
 </PREMISES>
 </INPUT>
+
 <OUTPUT>
 Premise: Harry is a human.
 FOL: Person(Harry)
-Premise: Walden is a book.
-FOL: Book(Walden)
 </OUTPUT>
 
 <INPUT>
 <PREMISES>
-Premise: Heinrich Schmidt was a Nazi German politician.
+Premise: Heinrich Schmidt was a Nazi German
+politician.
 FOL: NaziGermanPolitician (HeinrichSchmidt)
 </PREMISES>
 </INPUT>
+
 <OUTPUT>
 Premise: Heinrich Schmidt was a Nazi
 FOL: Nazi(HeinrichSchmidt)
@@ -150,7 +161,7 @@ Premise: Cold is not warm
 FOL: Cold -> -Warm
 </OUTPUT>
 
-Do not limit on how many additional premises you generate, so long as they are common sense.
+After generating this output pick the top 5 premises that are the most relevant to the provided premises, making sure to prune any that contradict each other or the given input. Return these with the tags <CONTEXT> and </CONTEXT> in the same format as the output.
 """
 
 
@@ -158,7 +169,8 @@ class CustomOAIEvaluator(OAIEvaluator):
     def generate_text(self, task):
         dataset = task.get_dataset()
         n_tasks = self.args.limit if self.args.limit else len(dataset)
-        prompts = [task.get_prompt(dataset[i]) for i in range(n_tasks)]
+        indices = list(range(n_tasks))
+        prompts = [task.get_prompt(dataset[i]) for i in indices]
         stops = [task.stop_words for _ in range(n_tasks)]
 
         with ThreadPoolExecutor() as executor:
@@ -171,17 +183,27 @@ class CustomOAIEvaluator(OAIEvaluator):
             contexts = list(executor.map(self.get_completion, context_prompt, stops))
 
         # todo bad code. doesn't work for sample bigger than 1
+
+        def extract_context_from_raw(text):
+            # this is for the new context Ate added
+            try:
+                return text.split('<CONTEXT>')[1].split('</CONTEXT>')[0]
+            except Exception as e:
+                print("failed to get the <CONTEXT> part in", text)
+                print('returning all of the generated thing')
+                return text
+
         generations_prc = [
             [
                 task.postprocess_generation(
-                    contexts[i][j] + '\n' + generations_raw[i][j], i, completion_only=True
+                    extract_context_from_raw(contexts[i][j]) + '\n' + generations_raw[i][j], i, completion_only=True
                 )
                 for j in range(self.args.n_samples)
             ]
             for i in range(n_tasks)
         ]
         references = [task.get_reference(dataset[i]) for i in range(n_tasks)]
-        return generations_prc, generations_raw, contexts, references
+        return indices, generations_prc, generations_raw, contexts, references
 
     def generate_raw(self, task, doc):
         pass
@@ -191,7 +213,7 @@ class CustomOAIEvaluator(OAIEvaluator):
         if task.requires_execution and not self.allow_code_execution:
             raise ValueError(_WARNING)
 
-        generations_prc, generations_raw, contexts, references = self.generate_text(task)
+        indices, generations_prc, generations_raw, contexts, references = self.generate_text(task)
         if len(generations_prc[0]) != self.args.n_samples:
             generations_prc = [l[: self.args.n_samples] for l in generations_prc]
             warnings.warn(
@@ -216,7 +238,10 @@ class CustomOAIEvaluator(OAIEvaluator):
                     with open(self.args.save_references_path, "w") as fp:
                         json.dump(references, fp)
                         print("references were saved")
-
+                if self.args.save_dataset_indices:
+                    with open(self.args.save_dataset_indices_path, "w") as fp:
+                        json.dump({"task_name": self.args.task_name, "indices": indices}, fp)
+                        print("dataset indices were saved")
             os.environ["TOKENIZERS_PARALLELISM"] = "false"
             if self.allow_code_execution and task.requires_execution:
                 os.environ["HF_ALLOW_CODE_EVAL"] = "1"
